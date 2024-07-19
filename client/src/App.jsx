0f6c37b89040e5b9d6ea2c13eca51364fb1a6331
Infinity375 <rostislav.delchev@gmail.com>
2024-07-19 21:32:19 +0300

    Separate html components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import Header from './components/header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div id="box">
+        <Header/>
 
+        <main id="main-content">
+
+
+
+        </main>
+
+      </div>
     </>
   )
 }
