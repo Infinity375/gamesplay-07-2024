e3fef1218a13f024d59f5601903cdbfcf6e5c5ca
Infinity375 <rostislav.delchev@gmail.com>
2024-07-31 20:43:20 +0300

    Add no games message

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -24,10 +24,15 @@ export default function Home() {
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {games.map(game => <GameListItem key={game._id} {...game} />)}
+
+            {games.length > 0
+                ?  games.map(game => <GameListItem key={game._id} {...game} />) 
+                : <h3 className="no-articles">No games yet</h3>
+            }
 
 
-            <h3 className="no-articles">No articles yet</h3>
+
+
         </section>
     )
 }
